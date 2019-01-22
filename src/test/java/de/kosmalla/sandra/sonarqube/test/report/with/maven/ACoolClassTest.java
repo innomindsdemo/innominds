a3562fc5be7e71611966ead4f217fa105545c76d
innomindsdemo <46806977+innomindsdemo@users.noreply.github.com>
2019-01-22 12:18:17 +0530

    Update ACoolClassTest.java

--- src/test/java/de/kosmalla/sandra/sonarqube/test/report/with/maven/ACoolClassTest.java
+++ src/test/java/de/kosmalla/sandra/sonarqube/test/report/with/maven/ACoolClassTest.java
@@ -16,6 +16,7 @@ public class ACoolClassTest {
         String greetz = coolClass.greetz();
         
         assertTrue(greetz.contains("unit test"));
+        assertfalse(greetz.contains("unittest"));
     }
   
 }
